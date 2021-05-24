58f590e42e56a1e59463cc6d16833311becf5eeb
Floozutter <me@floozutter.site>
2021-05-23 21:30:26 -0700

    use commander and add --asfile

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
+import {Command} from "commander";
 import {Client} from "discord.js";
+import {readFileSync} from "fs";
+
+const program = new Command();
+program
+.requiredOption("--token <string>", "Discord bot token")
+.option("--asfile", "interpret token <string> as file", false);
+program.parse();
+const opts = program.opts();
+const token = opts.asfile ? readFileSync(opts.token, "utf8") : opts.token;
+console.log(token);
 
 const client = new Client();
-
 client.on("ready", () => {
     console.log(`logged in as ${client.user.tag}.`);
 });
-
 client.on("message", msg => {
     if (msg.content === ">ping") {
         msg.reply("pong!");
     }
 });
-
-client.login(process.argv[2]);
+client.login(token);
