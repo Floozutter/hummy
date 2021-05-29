7b8d2c3b372e130e84810d5778d7ae85cea102fe
Floozutter <me@floozutter.site>
2021-05-29 00:25:52 -0700

    allow bot commands

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ client.once("ready", () => {
     console.log(`logged in as ${client.user?.tag ?? "???"}.`);
 });
 client.on("message", message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
     const [command, ...args] = message.content.slice(prefix.length).trim().split(' ');
     if (command === "ping") {
         message.reply("ping!");
